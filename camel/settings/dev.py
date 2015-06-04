8fa9afdddb4c882283d69a5b33f3548617566c97
Sean Powell <bendtsenpowell@gmail.com>
2015-06-04 12:53:04 +0100

    Added debug toolbar to dev req/settings

--- camel/settings/dev.py
+++ camel/settings/dev.py
@@ -2,9 +2,11 @@ from .base import * # noqa
 
 DATABASES = {
     'default': {
-	    'ENGINE': 'django.db.backends.sqlite3',
-	    'NAME': os.path.join(BASE_DIR, 'data') + 'camel.db',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'data') + 'camel.db',
     }
 }
 
 SECRET_KEY = 'a_secret_key'
+
+INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
