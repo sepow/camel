8324e9b8b92d793e5023c12e863d5896310ef39e
Sean Powell <bendtsenpowell@gmail.com>
2015-06-04 11:05:47 +0100

    Cleaning

---
+++ camel/settings/dev.py
@@ -0,0 +1,10 @@
+from .base import * # noqa
+
+DATABASES = {
+    'default': {
+	    'ENGINE': 'django.db.backends.sqlite3',
+	    'NAME': os.path.join(BASE_DIR, 'data') + 'camel.db',
+    }
+}
+
+SECRET_KEY = 'a_secret_key'
--- camel/urls.py
+++ camel/urls.py
@@ -8,9 +8,9 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 admin.autodiscover()
 
-from camel import settings
+from django.conf import settings
 
-import camel.views
+import core.views
 
 from django.views.generic import ListView
 from models import Module
@@ -18,40 +18,40 @@ from models import Module
 urlpatterns = patterns('',
 
     # basic
-    url(r'^$', camel.views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/camel_logo.png')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
 	# list views
-    url(r'^modules/$', camel.views.Module_ListView.as_view(), name='module-list'),
+    url(r'^modules/$', views.Module_ListView.as_view(), name='module-list'),
 
 	# detail views
-    url(r'^module/(?P<pk>\d+)/$', camel.views.Module_DetailView.as_view(), name="module-detail"),
-    url(r'^book/(?P<pk>\d+)/$', camel.views.Book_DetailView.as_view(), name="book-detail"),
-    url(r'^chapter/(?P<pk>\d+)/$', camel.views.Chapter_DetailView.as_view(), name="chapter-detail"),
-    url(r'^booknode/(?P<pk>\d+)/$', camel.views.BookNode_DetailView.as_view(), name="booknode-detail"),
+    url(r'^module/(?P<pk>\d+)/$', views.Module_DetailView.as_view(), name="module-detail"),
+    url(r'^book/(?P<pk>\d+)/$', views.Book_DetailView.as_view(), name="book-detail"),
+    url(r'^chapter/(?P<pk>\d+)/$', views.Chapter_DetailView.as_view(), name="chapter-detail"),
+    url(r'^booknode/(?P<pk>\d+)/$', views.BookNode_DetailView.as_view(), name="booknode-detail"),
 
     # selected node (by type)
-    url(r'^chapter/(?P<node_type>\w+)/(?P<pk>\d+)/$', camel.views.selected, name="chapter-selected"),
+    url(r'^chapter/(?P<node_type>\w+)/(?P<pk>\d+)/$', views.selected, name="chapter-selected"),
 
     # homework
-    url(r'^homework/(?P<pk>\d+)/$', camel.views.homework, name="homework"),
-    url(r'^answers/new/$', camel.views.edit_answer, name='new-answer',),
-    url(r'^answers/edit/(?P<pk>\d+)/$', camel.views.edit_answer, name='edit-answer',),
+    url(r'^homework/(?P<pk>\d+)/$', views.homework, name="homework"),
+    url(r'^answers/new/$', views.edit_answer, name='new-answer',),
+    url(r'^answers/edit/(?P<pk>\d+)/$', views.edit_answer, name='edit-answer',),
 
     # multiple choice
-    url(r'^sctest/(?P<pk>\d+)/$', camel.views.sctest, name="sctest"),
-    # url(r'mctest/(?P<pk>\d+)/$', camel.views.mctest, name="mctest"),
+    url(r'^sctest/(?P<pk>\d+)/$', views.sctest, name="sctest"),
+    # url(r'mctest/(?P<pk>\d+)/$', views.mctest, name="mctest"),
 
-	# users 
-	url(r'^home/(?P<pk>\d+)/$', camel.views.userhome, name="user-home"),
-	url(r'^login/$', camel.views.login_view, name="login"),
-    url(r'^logout/$', camel.views.logout_view, name="logout"),
-   
+	# users
+	url(r'^home/(?P<pk>\d+)/$', views.userhome, name="user-home"),
+	url(r'^login/$', views.login_view, name="login"),
+    url(r'^logout/$', views.logout_view, name="logout"),
+
     # search
-    url(r'^search-form/$', camel.views.search_form),
-    url(r'^search/$', camel.views.search),
+    url(r'^search-form/$', views.search_form),
+    url(r'^search/$', views.search),
 
 )
 
@@ -60,5 +60,3 @@ if settings.DEBUG:
     urlpatterns += patterns('',
         url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,}),
     )
-    
-
